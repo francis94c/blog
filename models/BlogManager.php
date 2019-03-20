3165e93dc29659c8992f9ef665d7e95111058c44
Francis Ilechukwu <francis94c@gmail.com>
2019-03-20 16:54:47 +0100

    createPost and savePost methods.

--- models/BlogManager.php
+++ models/BlogManager.php
@@ -3,5 +3,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class BlogManager extends CI_Model {
 
+  /**
+   * [createPost description]
+   * @param  [type] $title   [description]
+   * @param  [type] $content [description]
+   * @param  [type] $adminId [description]
+   * @return [type]          [description]
+   */
+  function createPost($title, $content, $adminId=null) {
+    $data = array(
+      "title"   => $title,
+      "content" => $content
+    );
+    if ($adminId != null) $data["poster_id"] = $adminId;
+    return $this->db->insert("blogger_posts", $data);
+  }
+  function getPosts($page, $limit) {
+    $offset = $page * $limit;
+    if ($limit != 0) $this->db->limit($offset, $limit);
+  }
 }
 ?>
