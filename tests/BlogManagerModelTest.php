16a25b886a7192f7b9326ab3f3b6dd9cd6556280
Francis Ilechukwu <francis94c@gmail.com>
2019-03-22 09:33:50 +0100

    savePost() and getPost() test.

--- tests/BlogManagerModelTest.php
+++ tests/BlogManagerModelTest.php
@@ -9,10 +9,14 @@ class BlogManagerModelTest {
     $ci->load->splint(self::PACKAGE, "+Blogger", null, "blogger");
     $ci->unit->run($ci->blogger->install("admins", "id", 1), true, "Install Blog Database.");
     $ci->load->splint(self::PACKAGE, "*BlogManager", "bmanager");
-    $ci->unit->run($ci->bmanager->createPost("Hello", "World", 1) !== false, true, "Create Post");
+    $postId = $ci->bmanager->createPost("Hello", "World", 1);
+    $ci->unit->run($postId !== false, true, "Create Post");
     $ci->unit->run(is_numeric($ci->bmanager->getPosts(0, 0)[0]["id"]), true, "ID Check");
-    $ci->unit->run($ci->bmanager->getPosts(0, 0)[0]["title"], "Hello", "Title Check");
-    $ci->unit->run($ci->bmanager->getPosts(0, 0)[0]["content"], "World", "Content Check");
+    $ci->unit->run($ci->bmanager->getPost($postId)["title"], "Hello", "Title Check");
+    $ci->unit->run($ci->bmanager->getPost($postId)["content"], "World", "Content Check");
+    $ci->unit->run($ci->bmanager->savePost($postId, "Hello Title", "Hello Content"), true, "Save Post.");
+    $ci->unit->run($ci->bmanager->getPost($postId)["title"], "Hello Title", "Title Check");
+    $ci->unit->run($ci->bmanager->getPost($postId)["content"], "Hello Content", "Content Check");
     $this->cleanUp($ci);
   }
   private function cleanUp(&$ci) {
