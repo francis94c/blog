16a4bf8c5d5f959dd5be4f15f0dabf2d7d399890
Francis Ilechukwu <francis94c@gmail.com>
2019-03-27 19:20:59 +0100

    renderPostItems() test.

--- tests/BlogTest.php
+++ tests/BlogTest.php
@@ -10,12 +10,23 @@ class BlogTest {
     $ci->unit->run($ci->blogger->install("", "admins", "id", 1), true, "Install Blog Database.");
     $ci->unit->run($ci->blogger->install("test_name", "admins", "id", 1), true, "Install Test Blog Database.");
     $ci->blogger->loadEditor("callback");
+    $oldVal = $_POST["action"];
+    $_POST["action"] = "save";
+    unset($_POST["id"]);
+    $ci->blogger->renderPostItems(null, null, null, 1, 0);
+    $ci->blogger->renderPostItems(null, null, "welcome_message", 1, 0);
+    $ci->blogger->savePost(1);
+    $ci->blogger->savePost(1);
+    $ci->blogger->renderPostItems(null, null, null, 1, 0);
+    $ci->blogger->renderPostItems("welcome_message", "welcome_message", null, 1, 0);
+    $_POST["action"] = $oldVal;
     $this->cleanUp($ci);
   }
   function libTest(&$ci) {
     $ci->load->splint("francis94c/blog", "+Blogger", null, "blogger");
     $ci->blogger->setName("test_name");
     $ci->unit->run($ci->blogger->getName(), Blogger::TABLE_PREFIX . "_test_name", "Test Blogger setName()");
+    $this->cleanUp($ci);
   }
   function createPostTest(&$ci) {
     $ci->load->splint("francis94c/blog", "+Blogger", null, "blogger");
