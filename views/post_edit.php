5f657504d05ba75398c2b9dd00bbc8f1fa4b41f3
Francis Ilechukwu <francis94c@gmail.com>
2019-04-23 06:40:18 +0100

    UI and empty delete functions.

--- views/post_edit.php
+++ views/post_edit.php
@@ -3,10 +3,10 @@ if (!isset($title)) $title = "";
 if (!isset($content)) $content = "";
 if (!isset($prompt_color)) $prompt_color = "w3-green";
 ?>
-<div id="modal" class="w3-modal w3-animate-opacity">
+<div id="publishModal" class="w3-modal w3-animate-opacity">
   <div class="w3-modal-content">
     <header class="w3-container <?=$prompt_color?>">
-      <span onclick="document.getElementById('modal').style.display = 'none'" class="w3-button w3-hover-blue w3-display-topright">
+      <span onclick="document.getElementById('publishModal').style.display = 'none'" class="w3-button w3-hover-blue w3-display-topright">
         <i class="fa fa-times"></i>
       </span>
       <h2>Publish?</h2>
@@ -14,8 +14,25 @@ if (!isset($prompt_color)) $prompt_color = "w3-green";
     <div class="w3-container">
       <p>Are you sure you want to publish this content?</p>
       <div class="w3-right">
-        <button onclick="document.getElementById('modal').style.display = 'none'" class="w3-margin w3-button w3-hover-blue w3-green">No</button>
-        <button onclick="document.getElementById('modal').style.display = 'none';saveAndPublish();" class="w3-margin w3-button w3-hover-blue w3-green">Yes</button>
+        <button onclick="document.getElementById('publishModal').style.display = 'none'" class="w3-margin w3-button w3-hover-blue w3-green">No</button>
+        <button onclick="document.getElementById('publishModal').style.display = 'none';saveAndPublish();" class="w3-margin w3-button w3-hover-blue w3-green">Yes</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="deleteModal" class="w3-modal w3-animate-opacity">
+  <div class="w3-modal-content">
+    <header class="w3-container w3-red">
+      <span onclick="document.getElementById('deleteModal').style.display = 'none'" class="w3-button w3-hover-blue w3-display-topright">
+        <i class="fa fa-times"></i>
+      </span>
+      <h2>Publish?</h2>
+    </header>
+    <div class="w3-container">
+      <p>Are you sure you want to delete this content?</p>
+      <div class="w3-right">
+        <button onclick="document.getElementById('deleteModal').style.display = 'none'" class="w3-margin w3-button w3-hover-blue w3-green">No</button>
+        <button onclick="document.getElementById('deleteModal').style.display = 'none';delete();" class="w3-margin w3-button w3-hover-blue w3-red">Yes</button>
       </div>
     </div>
   </div>
@@ -49,19 +66,34 @@ simpleMDE.value(document.getElementById("content").innerHTML);
 //var markdown = simpleMDE.value();
 //markdown = markdown.replace(/    /g, "  \r\n");
 //simpleMDE.value(markdown);
-
+/**
+ * [save description]
+ * @return [type] [description]
+ */
 function save() {
   document.getElementById("action").value = "save";
   document.getElementById("postForm").submit();
 }
-
+/**
+ * [saveAndPublish description]
+ * @return [type] [description]
+ */
 function saveAndPublish() {
   document.getElementById("action").value = <?=$type == "edit" ? "\"publish\"" : "\"createAndPublish\"";?>;
   document.getElementById("postForm").submit();
 }
-
+/**
+ * [promptPublish description]
+ * @return [type] [description]
+ */
 function promptPublish() {
-  document.getElementById("modal").style.display = "block";
+  document.getElementById("publishModal").style.display = "block";
+}
+function promptDelete() {
+
+}
+function delete() {
+  
 }
 </script>
 <input type="hidden" name="id" value="<?=$type == "edit" ? $id : ""?>"/>
@@ -69,3 +101,6 @@ function promptPublish() {
 <?=form_close();?>
 <button onclick="save();" class="w3-margin w3-button w3-teal w3-round w3-hover-theme"><?=$type == "edit" ? "Save" : "Create"?></button>
 <button onclick="promptPublish();" class="w3-margin w3-button w3-teal w3-hover-theme w3-round"><?=$type == "edit" ? "Save and Publish" : "Create and Publish"?></button>
+<?php if ($type == "edit") {?>
+<button onclick="promptDelete();" class="w3-margin w3-button w3-red w3-hover-theme w3-round">Delete</button>
+<?php }?>
