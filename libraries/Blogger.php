3c501f9aec6fa755d92a5cd8d131a35eb821f0c2
Francis Ilechukwu <francis94c@gmail.com>
2019-06-15 18:35:10 +0100

    Elipsize post items.

--- libraries/Blogger.php
+++ libraries/Blogger.php
@@ -216,10 +216,11 @@ class Blogger {
         return;
       }
     }
+    $this->ci->load->helper("text");
     foreach ($posts as $post) {
       $post["callback"] = $callback != null ? trim($callback, "/") . "/" . ($slug ? $post["slug"] : $post["id"]) : "";
       $post["filter"] = $filter;
-      $post["content"] = $this->ci->parsedown->text($post["content"]);
+      $post["content"] = $this->ci->parsedown->text(ellipsize($post["content"], 300));
       if ($view == null) {$blogger->load->view("post_list_item", $post); } else {
         $this->ci->load->view($view, $post);
       }
