50d0b02a4af00c9380098f454fbc40171f8193fe
Francis Ilechukwu <francis94c@gmail.com>
2019-06-23 22:22:37 +0100

    Curly brackets

--- travis.sh
+++ travis.sh
@@ -9,25 +9,22 @@ created=false
 for entry in ./*
 do
 	if [ $created = false ]; then
-		mkdir -p travis-splint-$SPLINT_VERSION/application/splints/
+		mkdir -p travis-splint-${SPLINT_VERSION}/application/splints/${VENDOR}/${PACKAGE}
 		$created = true
 	fi
-	if [ "x$entry" != "x./$VENDOR/$PACKAGE/phpunit.xml" ] && [ "x$entry" != "x./$VENDOR/$PACKAGE/travis.sh" ]; then
-		echo $entry
-		cp -r $entry travis-splint-$SPLINT_VERSION/application/splints/
+	if [ "x$entry" != "x./phpunit.xml" ] && [ "x$entry" != "x./travis.sh" ]; then
+		cp -r $entry travis-splint-${SPLINT_VERSION}/application/splints/${VENDOR}/${PACKAGE}/
 		rm -rf $entry
 	fi
 done
 
-cp "$VENDOR/$PACKAGE/phpunit.xml" "../../phpunit.xml"
-
-wget https://github.com/splintci/travis-splint/archive/v$SPLINT_VERSION.tar.gz -O - | tar xz
+wget https://github.com/splintci/travis-splint/archive/v${SPLINT_VERSION}.tar.gz -O - | tar xz
 
 # Dependencies
 
 wget https://github.com/francis94c/ci-parsedown/archive/v0.0.2.tar.gz -O - | tar xz
 
 mv ci-parsedown-0.0.2 ci-parsedown
-mkdir -p travis-splint-$SPLINT_VERSION/application/splints/francis94c/
-cp -r ci-parsedown travis-splint-$SPLINT_VERSION/application/splints/francis94c/
+mkdir -p travis-splint-${SPLINT_VERSION}/application/splints/francis94c/
+cp -r ci-parsedown travis-splint-${SPLINT_VERSION}/application/splints/francis94c/
 rm -rf ci-parsedown
