75c5906481adcf5d78c2ff235439f9f805ef04bc
Francis Ilechukwu <francis94c@gmail.com>
2019-06-23 20:56:45 +0100

    Test Dependencies.

--- travis.sh
+++ travis.sh
@@ -19,3 +19,12 @@ do
 done
 
 wget https://github.com/splintci/travis-splint/archive/v$SPLINT_VERSION.tar.gz -O - | tar xz
+
+# Dependencies
+
+wget https://github.com/francis94c/ci-parsedown/archive/v0.0.2.tar.gz -O - | tar xz
+
+mv ci-parsedown-0.0.2 ci-parsedown
+mkdir -p travis-splint-$SPLINT_VERSION/application/splints/francis94c/
+cp -r ci-parsedown travis-splint-$SPLINT_VERSION/application/splints/francis94c/
+rm -rf ci-parsedown
