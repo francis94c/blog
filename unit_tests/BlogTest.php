4dcb483e2ba0c95de52b2619cb286e1c86fc4470
Francis Ilechukwu <francis94c@gmail.com>
2019-06-23 23:11:40 +0100

    Query.

--- unit_tests/BlogTest.php
+++ unit_tests/BlogTest.php
@@ -7,6 +7,7 @@ final class BlogTest extends TestCase {
     $ci =& get_instance();
     $ci->load->splint("francis94c/blog", "+Blogger", null, "blogger");
     $ci->load->database();
+    $ci->db->query("USE test_db;");
     $this->assertTrue($ci->blogger->install("test_blog"));
   }
 }
